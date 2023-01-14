9d951699ac9d1ad2c6f8fc35961c21936fed3945
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-14 18:15:57 +0100

    cms

---
+++ fin_1er_trimestre/js/main.js
@@ -0,0 +1,49 @@
+/*
+  @author: Christian Millán Soria
+  description: main script
+*/
+
+"use strict"
+
+// automatic slideshow
+  var slideIndex=0;
+
+  var auto;
+
+  var time=3750;
+
+  function plusSlides(n){
+    showSlides(slideIndex+=n);
+
+    clearTimeout(auto);
+  }
+
+  showSlides();
+    function showSlides(){
+      var i;
+
+      var slides=document.getElementsByClassName("mySlides");
+
+      for(i=0; i<slides.length; i++){
+        slides[i].style.display="none"; 
+      }
+
+      slideIndex++;
+
+      if (slideIndex>slides.length){
+        slideIndex=1
+      }
+
+      var rand=Math.floor((Math.random()*slides.length));
+
+      slides[rand].style.display = "block"; 
+
+      auto=setTimeout(showSlides, time);
+    }
+
+/***********************************************************/
+
+// loader
+  $(window).load(function() {
+    $(".loader").fadeOut("slow");
+  });
---
+++ fin_1er_trimestre/js/planets.js
@@ -0,0 +1,11 @@
+/*
+  @author: Christian Millán Soria
+  description: planets script
+*/
+
+"use strict"
+
+// loader
+  $(window).load(function(){
+    $(".loader").fadeOut("slow");
+  });
