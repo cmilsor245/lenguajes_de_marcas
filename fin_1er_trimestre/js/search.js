73d0bde6899aab5e03c96e94da9fb0d637b76fd9
cmilsor245 <cmilsor245@g.educaand.es>
2023-01-17 07:48:27 +0100

    cms

--- fin_1er_trimestre/js/search.js
+++ fin_1er_trimestre/js/search.js
@@ -12,14 +12,72 @@
   searchForm.addEventListener("submit", e=>{
     e.preventDefault();
     const searchValue=searchInput.value.toLowerCase();
-    if (searchValue==="characters"){
+    if(searchValue==="characters"){
       window.location.href="pages/characters.html";
-    } else if (searchValue==="media"){
+    }else if(searchValue==="media"){
       window.location.href="pages/media.html"; 
-    } else if (searchValue==="planets"){
+    }else if(searchValue==="planets"){
       window.location.href="pages/planets.html";
-    } else{
-      alert("Page not found. Please try again.");
+
+    // media
+    }else if(searchValue==="the phantom menace"||searchValue==="episode 1"||searchValue==="tpm"){
+      window.location.href="pages/sub-pages/media/episode1.html";
+    }else if(searchValue==="attack of the clones"||searchValue==="episode 2"||searchValue==="aotc"){
+      window.location.href="pages/sub-pages/media/episode2.html";
+    }else if(searchValue==="the clone wars"||searchValue==="tcw"){
+      window.location.href="pages/sub-pages/media/the_clone_wars.html";
+    }else if(searchValue==="revenge of the sith"||searchValue==="episode 3"||searchValue==="rots"){
+      window.location.href="pages/sub-pages/media/episode3.html";
+    }else if(searchValue==="solo"){
+      window.location.href="pages/sub-pages/media/solo.html";
+    }else if(searchValue==="rogue one"){
+      window.location.href="pages/sub-pages/media/rogue_one.html";
+    }else if(searchValue==="a new hope"||searchValue==="episode 4"||searchValue==="anh"){
+      window.location.href="pages/sub-pages/media/episode4.html";
+    }else if(searchValue==="the empire strikes back"||searchValue==="episode 5"||searchValue==="tesb"){
+      window.location.href="pages/sub-pages/media/episode5.html";
+    }else if(searchValue==="return of the jedi"||searchValue==="episode 6"||searchValue==="rotj"){
+      window.location.href="pages/sub-pages/media/episode6.html";
+    }else if(searchValue==="the force awakens"||searchValue==="episode 7"||searchValue==="tfa"){
+      window.location.href="pages/sub-pages/media/episode7.html";
+    }else if(searchValue==="the last jedi"||searchValue==="episode 8"||searchValue==="tlj"){
+      window.location.href="pages/sub-pages/media/episode8.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+
+    // characters
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+
+    // planets
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
+    }else if(searchValue==="the rise of skywalker"||searchValue==="episode 9"||searchValue==="tros"){
+      window.location.href="pages/sub-pages/media/episode9.html";
     }
   });
 
